cf4ba5b2e65da5914237f8005bf0ae9f3ee21375
King <nigus.kidane@gmail.com>
2019-10-12 00:29:44 -0600

    push(), pop(), peek(), isEmpty(),size() methods with there respective test added

--- src/main/java/io/nk/StackWithSLLTDD/StackSLL.java
+++ src/main/java/io/nk/StackWithSLLTDD/StackSLL.java
@@ -2,7 +2,35 @@ package io.nk.StackWithSLLTDD;
 
 public class StackSLL {
 
+    private Object object;
+    private int size;
+
     public boolean isEmpty() {
-        return true;
+        return object == null;
+    }
+
+    public void push(Object ob) {
+        size++;
+        object = ob;
+    }
+
+    public Object pop() throws Exception {
+        if(isEmpty()){
+            throw new Exception();
+        }
+        Object result = object;
+        object = null;
+        return result;
+    }
+
+    public Object peek() throws Exception {
+        if(isEmpty()){
+            throw new Exception();
+        }
+        return object;
+    }
+
+    public int size() {
+        return size;
     }
 }
--- src/test/java/io/nk/StackWithSLLTDD/StackTest.java
+++ src/test/java/io/nk/StackWithSLLTDD/StackTest.java
@@ -1,7 +1,6 @@
 package io.nk.StackWithSLLTDD;
 
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,17 +15,68 @@ import static junit.framework.TestCase.assertEquals;
 public class StackTest {
 
     private StackSLL stack;
-
+    Object ob = "test";
     @Before
     public void setUp() {
         stack = new StackSLL();
     }
 
     @Test
-    public void testStack_isEmpty(){
+    public void testStack_isEmpty() {
         assertEquals(true, stack.isEmpty());
     }
 
+    private void pushingOneElementInToTheStack(Object ob) {
+        stack.push(ob);
+    }
 
+    private void pushingManyElementInToTheStack(Object ...ob) {
+        for(Object o:ob){
+            stack.push(o);
+        }
+    }
+
+    @Test
+    public void testStack_pushingOneElement() {
+        pushingOneElementInToTheStack(ob);
+        assertEquals(false, stack.isEmpty());
+    }
+
+    @Test
+    public void testStack_pushingManyOneElement() {
+        pushingManyElementInToTheStack("Seli","Nigus","Anna","Bob");
+        assertEquals(4, stack.size());
+    }
+
+    @Test(expected = Exception.class)
+    public void testStack_popWithNoValue() throws Exception {
+        popingOneElementFromTheStack();
+    }
+
+    @Test
+    public void testStack_pop() throws Exception {
+        pushingOneElementInToTheStack(ob);
+        popingOneElementFromTheStack();
+        assertEquals(true, stack.isEmpty());
+    }
+
+    @Test(expected = Exception.class)
+    public void testStack_peekWithNoElement() throws Exception{
+        peekingFromTheStack();
+    }
+
+    @Test
+    public void testStack_peekWithElement() throws Exception{
+        pushingOneElementInToTheStack(ob);
+        peekingFromTheStack();
+    }
+
+    private void peekingFromTheStack() throws Exception {
+        stack.peek();
+    }
+
+    private void popingOneElementFromTheStack() throws Exception {
+        stack.pop();
+    }
 
 }
