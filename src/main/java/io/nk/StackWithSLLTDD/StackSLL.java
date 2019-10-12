e90a88f7ea9c118b18c2b6f7818d50782d0ffd31
King <nigus.kidane@gmail.com>
2019-10-12 02:02:17 -0600

    Node is created and methods are added based on that

--- src/main/java/io/nk/StackWithSLLTDD/StackSLL.java
+++ src/main/java/io/nk/StackWithSLLTDD/StackSLL.java
@@ -1,36 +1,89 @@
 package io.nk.StackWithSLLTDD;
 
-public class StackSLL {
+public class StackSLL<T> {
 
-    private Object object;
-    private int size;
+    private Node first;
 
     public boolean isEmpty() {
-        return object == null;
+        return first == null;
     }
 
-    public void push(Object ob) {
-        size++;
-        object = ob;
+    public void push(T ob) {
+        if (isEmpty()) {
+            first = new Node(ob);
+        } else {
+            Node temp = first;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(ob);
+        }
+
     }
 
-    public Object pop() throws Exception {
-        if(isEmpty()){
+    public T pop() throws Exception {
+        if (isEmpty()) {
             throw new Exception();
         }
-        Object result = object;
-        object = null;
-        return result;
+
+        Node last = first;
+        Node previousNode = null;
+        while (last.next != null) {
+            previousNode = last;
+            last = last.next;
+        }
+        if (previousNode == null) {
+            last = first;
+            first = null;
+        } else {
+            previousNode.next = null;
+        }
+        return last.value;
     }
 
     public Object peek() throws Exception {
-        if(isEmpty()){
+        if (isEmpty()) {
             throw new Exception();
         }
-        return object;
+        Node last = first;
+        while (last.next != null) {
+            last = last.next;
+        }
+        return last.value;
     }
 
     public int size() {
-        return size;
+        int count = 0;
+        Node temp = first;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public boolean contains(T ob) {
+        boolean contains = false;
+        if (isEmpty()) {
+            return contains;
+        }
+        Node temp = first;
+        while (temp != null) {
+            if (temp.value.equals(ob)) {
+                contains = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        return contains;
+    }
+
+    private class Node {
+        private T value;
+        private Node next;
+
+        public Node(T value) {
+            this.value = value;
+        }
     }
 }
--- src/test/java/io/nk/StackWithSLLTDD/StackTest.java
+++ src/test/java/io/nk/StackWithSLLTDD/StackTest.java
@@ -11,15 +11,12 @@ import static junit.framework.TestCase.assertEquals;
 //push()
 //pop()
 //peek()
+//contains
 
 public class StackTest {
 
-    private StackSLL stack;
+    private StackSLL<Object> stack=new StackSLL();;
     Object ob = "test";
-    @Before
-    public void setUp() {
-        stack = new StackSLL();
-    }
 
     @Test
     public void testStack_isEmpty() {
@@ -54,10 +51,21 @@ public class StackTest {
     }
 
     @Test
-    public void testStack_pop() throws Exception {
+    public void testStack_popWithOneValue() throws Exception {
         pushingOneElementInToTheStack(ob);
         popingOneElementFromTheStack();
         assertEquals(true, stack.isEmpty());
+    }
+
+    @Test
+    public void testStack_popingFromManyElement() throws Exception {
+        pushingManyElementInToTheStack("Seli","Nigus","Anna","Bob");
+
+        assertEquals("Bob", stack.pop());
+        assertEquals(3, stack.size());
+        assertEquals("Anna", stack.pop());
+        assertEquals(2, stack.size());
+
     }
 
     @Test(expected = Exception.class)
@@ -69,6 +77,31 @@ public class StackTest {
     public void testStack_peekWithElement() throws Exception{
         pushingOneElementInToTheStack(ob);
         peekingFromTheStack();
+    }
+
+    @Test
+    public void testStack_peekingFromManyElement() throws Exception {
+        pushingManyElementInToTheStack("Seli","Nigus","Anna","Bob");
+        assertEquals("Bob", stack.peek());
+        assertEquals(4, stack.size());
+    }
+
+    @Test
+    public void testStack_containsWithNoElement() {
+        assertEquals(false, stack.contains(ob));
+    }
+
+    @Test
+    public void testStack_containsWithOneElement() {
+        pushingOneElementInToTheStack(ob);
+        assertEquals(true, stack.contains(ob));
+    }
+
+    @Test
+    public void testStack_containsWithManyElement() {
+        pushingManyElementInToTheStack("Seli","Nigus","Anna","Bob");
+        assertEquals(true, stack.contains("Seli"));
+        assertEquals(true, stack.contains("Bob"));
     }
 
     private void peekingFromTheStack() throws Exception {
